f599938f736a188767749f28db5a70d9811cbe70
linux_china <libing.chen@gmail.com>
2022-07-27 14:59:43 -0700

    refactor: extract configuration to LoomVirtualThreadCofiguration

--- src/main/java/com/example/loomservlet/ServletOfTheLoomApplication.java
+++ src/main/java/com/example/loomservlet/ServletOfTheLoomApplication.java
@@ -2,33 +2,12 @@ package com.example.loomservlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.task.AsyncTaskExecutor;
-import org.springframework.core.task.support.TaskExecutorAdapter;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class ServletOfTheLoomApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServletOfTheLoomApplication.class, args);
-    }
-
-    @Bean
-    AsyncTaskExecutor applicationTaskExecutor() {
-        // enable async servlet support
-        ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
-        return new TaskExecutorAdapter(executorService::execute);
-    }
-
-    @Bean
-    TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
-        return protocolHandler -> {
-            protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
-        };
     }
 
 }
